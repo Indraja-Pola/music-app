50cee197956dc13a10aefde8e5e1a2396a60b967
Indrajapola <146160553+Indrajapola@users.noreply.github.com>
2024-04-03 11:28:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import img from "./SoundImg/logo.png";
+import play from './SoundImg/play.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="navbar">
+        <img src= {img} className='logo'/>
+        <ul>
+          <li><a href='#'>HOME</a></li>
+          <li><a href='#'>ABOUT</a></li>
+          <li><a href='#'>SPECIFICATIONS</a></li>
+          <li><a href='#'>PRODUCTS</a></li>
+          <li><a href='#'>CONNECT</a></li>
+
+        </ul>
+      </div>
+  <div className='content'>
+    <div className='left-col'>
+      <h1>THE<br></br>REAL <br></br>SOUND</h1>
     </div>
+    <div className='right-col'>
+      <p>
+           Click Here To Listen
+    </p>
+    <img src={play} alt='play' className='icon'/>
+
+    </div>
+
+
+  </div>
+    </div>
+ 
+   
   );
 }
 
