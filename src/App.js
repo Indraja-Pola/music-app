d307fb642719b204b69b60b23c23869ec9c31abb
dev_test_vs <satish.i@outlook.com>
2026-01-22 13:40:11 +0000

    updated styles and music is added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 import img from "./SoundImg/logo.png";
-import play from './SoundImg/play.png';
+import play from "./SoundImg/play.png";
+import pause from "./SoundImg/pause.png";
+import music from "./SoundImg/mixkit-beautiful-dream-493.mp3";
 
 function App() {
+  const audioRef = useRef(new Audio(music)); // store audio object
+  const [isPlaying, setIsPlaying] = useState(false); // track play/pause
+
+  function togglePlay() {
+    if (!isPlaying) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  }
+  function showMessage() {
+    alert("Please play the music 🎵");
+  }
+
   return (
     <div className="container">
       <div className="navbar">
-        <img src= {img} className='logo'/>
+        <img src={img} className="logo" />
         <ul>
-          <li><a href='#'>HOME</a></li>
-          <li><a href='#'>ABOUT</a></li>
-          <li><a href='#'>SPECIFICATIONS</a></li>
-          <li><a href='#'>PRODUCTS</a></li>
-          <li><a href='#'>CONNECT</a></li>
-
+          <li>
+            <a onClick={showMessage}>HOME</a>
+          </li>
+          <li>
+            <a onClick={showMessage}>ABOUT</a>
+          </li>
+          <li>
+            <a onClick={showMessage}>SPECIFICATIONS</a>
+          </li>
+          <li>
+            <a onClick={showMessage}>PRODUCTS</a>
+          </li>
+          <li>
+            <a onClick={showMessage}>CONNECT</a>
+          </li>
         </ul>
       </div>
-  <div className='content'>
-    <div className='left-col'>
-      <h1>THE<br></br>REAL <br></br>SOUND</h1>
-    </div>
-    <div className='right-col'>
-      <p>
-           Click Here To Listen
-    </p>
-    <img src={play} alt='play' className='icon'/>
+      <div className="content">
+        <div className="left-col">
+          <h1>
+            THE<br></br>REAL <br></br>SOUND
+          </h1>
+        </div>
+        <div className="right-col">
+          <p>Click Here To Listen</p>
 
+          <img
+            src={isPlaying ? pause : play}
+            alt="toggle"
+            className="icon"
+            onClick={togglePlay}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+      </div>
     </div>
-
-
-  </div>
-    </div>
- 
-   
   );
 }
 
